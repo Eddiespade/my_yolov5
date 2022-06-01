bcf9538d06e0c5a0adef3c474e0e602838f1c7ae
Eddiespade <1315900790@qq.com>
2022-06-01 11:02:01 +0800

    项目初始化

--- train.py
+++ train.py
@@ -5,24 +5,39 @@ import torch  # Pytorch深度学习相关模块
 import yaml  # 操作yaml文件模块
 from pathlib import Path  # Path将str转换为Path对象 使字符串路径易于操作的模块
 
-from utils.utils import increment_path
+from models.yolo import Model
+from utils.utils import increment_path, select_device
 
 if __name__ == '__main__':
-    # opt设置文件
-    with open("configs/opt.yaml", encoding='utf-8') as f:
-        opt = yaml.safe_load(f)  # load hyps dict  加载超参信息
-    # 输出超参信息 opt: ...
-    # 显示颜色 print("\033[显示方式；前景颜色；背景颜色m 要打印的信息 \033[0m")
-    print('\033[0;31;40mopt:\033[0m ' + ', '.join(f'{k}={v} ' for k, v in opt.items()))
-
-    # 自增保存路径
-    save_dir = Path(str(increment_path(Path(opt['project']) / opt['name'])))
-    weights_dir = save_dir / 'weights'
-    last = weights_dir / 'last.pt'
-    best = weights_dir / 'best.pt'
-    results_file = save_dir / 'results.txt'
-
     # 初始化随机数种子, 以保证结果的一致性
     random.seed(0)
     np.random.seed(0)
     torch.manual_seed(0)
+
+    """ 以下为加载 configs下的 配置文件"""
+    with open("configs/opt.yaml", encoding='utf-8') as f:   # 读取opt设置文件 ---> 对应 yolov5中的 argparse
+        opt = yaml.safe_load(f)  # 加载设置信息
+    # 输出超参信息 opt: ... 显示颜色 print("\033[显示方式；前景颜色；背景颜色m 要打印的信息 \033[0m")
+    print('\033[0;31;40mopt:\033[0m ' + ', '.join(f'{k}={v} ' for k, v in opt.items()))
+
+    with open(opt['hyp'], encoding='utf-8') as f:           # Hyperparameters超参
+        hyp = yaml.safe_load(f)  # 加载超参信息
+    print('\033[0;31;40mHyperparameters:\033[0m ' + ', '.join(f'{k}={v} ' for k, v in opt.items()))
+
+    # data_dict: 加载VOC.yaml中的数据配置信息  dict
+    with open(opt['data'], encoding='utf-8') as f:
+        data_dict = yaml.safe_load(f)
+
+    # 自增保存路径
+    save_dir = Path(str(increment_path(Path(opt['project']) / opt['name'])))  # 保存训练结果的目录  如runs/train/exp18
+    weights_dir = save_dir / 'weights'  # 保存权重路径 如runs/train/exp18/weights
+    # 创建文件目录   参数说明：parents：如果父目录不存在，是否创建父目录。 exist_ok：只有在目录不存在时创建目录，目录已存在时不会抛出异常。
+    weights_dir.mkdir(parents=True, exist_ok=True)
+    last = weights_dir / 'last.pt'
+    best = weights_dir / 'best.pt'
+    results_file = save_dir / 'results.txt'
+
+    # 搭建模型
+    device = select_device(opt['device'], batch_size=opt['batch_size'])
+    nc = 1 if opt['single_cls'] else int(data_dict['nc'])  # number of classes
+    model = Model(opt['cfg'], ch=3, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,9 @@
+import platform
 from pathlib import Path  # Path将str转换为Path对象 使字符串路径易于操作的模块
 import re           # 用来匹配字符串（动态、模糊）的模块
 import glob         # 仅支持部分通配符的文件搜索模块
+import os
+import torch
 
 
 def increment_path(path, sep='', mkdir=False):
@@ -37,3 +40,54 @@ def increment_path(path, sep='', mkdir=False):
     if not dir.exists() and mkdir:  # mkdir 默认False 先不创建dir
         dir.mkdir(parents=True, exist_ok=True)  # make directory
     return path  # 返回runs/train/exp2
+
+
+def select_device(device='', batch_size=None):
+    """广泛用于train.py、val.py、detect.py等文件中
+    用于选择模型训练的设备 并输出日志信息
+    :params device: 输入的设备  device = 'cpu' or '0' or '0,1,2,3'
+    :params batch_size: 一个批次的图片个数
+    """
+    # git_describe(): 返回当前文件父文件的描述信息(yolov5)   date_modified(): 返回当前文件的修改日期
+    # s: 之后要加入logger日志的显示信息
+    s = f'\033[0;31;40mmy_yolov5: \033[0m' + f' torch {torch.__version__}  '  # string
+
+    # 如果device输入为cpu  cpu=True  device.lower(): 将device字符串全部转为小写字母
+    cpu = device.lower() == 'cpu'
+    if cpu:
+        # 如果cpu=True 就强制(force)使用cpu 令torch.cuda.is_available() = False
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    elif device:
+        # 如果输入device不为空  device=GPU  直接设置 CUDA environment variable = device 加入CUDA可用设备
+        os.environ['CUDA_VISIBLE_DEVICES'] = device
+        # 检查cuda的可用性 如果不可用则终止程序
+        assert torch.cuda.is_available(), f'CUDA unavailable, invalid device {device} requested'
+
+    # 输入device为空 自行根据计算机情况选择相应设备  先看GPU 没有就CPU
+    # 如果cuda可用 且 输入device != cpu 则 cuda=True 反正cuda=False
+    cuda = not cpu and torch.cuda.is_available()
+    if cuda:
+        # devices: 如果cuda可用 返回所有可用的gpu设备 i.e. 0,1,6,7  如果不可用就返回 '0'
+        devices = device.split(',') if device else '0'
+        # n: 所有可用的gpu设备数量  device count
+        n = len(devices)
+        # 检查是否有gpu设备 且 batch_size是否可以能被显卡数目整除  check batch_size is divisible by device_count
+        if n > 1 and batch_size:
+            # 如果不能则关闭程序
+            assert batch_size % n == 0, f'batch-size {batch_size} not multiple of GPU count {n}'
+
+        space = ' ' * (len(s) + 1)  # 定义等长的空格
+
+        # 满足所有条件 s加上所有显卡的信息
+        for i, d in enumerate(devices):
+            # p: 每个可用显卡的相关属性
+            p = torch.cuda.get_device_properties(i)
+            # 显示信息s加上每张显卡的属性信息
+            s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / 1024 ** 2}MB)\n"  # bytes to MB
+    else:
+        # cuda不可用显示信息s就加上CPU
+        s += 'CPU\n'
+
+    print(s)
+    # 如果cuda可用就返回第一张显卡的的名称 如: GeForce RTX 2060 反之返回CPU对应的名称
+    return torch.device('cuda:0' if cuda else 'cpu')
